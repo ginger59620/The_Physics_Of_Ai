56c00c9dd90f79baa6470056f1f14b15c7875fc4
ginger59620 <a59620@alunos.ipb.pt>
2025-10-01 21:26:34 +0100

    Initial commit

---
+++ Assets/Script/SecondsUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+    float timeStart0ffset = 0;
+    bool gotStartTime = false;
+
+    void Update()
+    {
+        if(!gotStartTime)
+        {
+            timeStart0ffset = Time.realtimeSinceStartup;
+            gotStartTime = true; 
+        }
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, Time.realtimeSinceStartup - timeStart0ffset);
+    }
+}
