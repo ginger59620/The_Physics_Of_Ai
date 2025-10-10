b0ea0cf832e1c587bd196c222fa81bdaab547440
ginger59620 <a59620@alunos.ipb.pt>
2025-10-10 10:56:32 +0100

    Half of part 3

--- Assets/Scripts/FireShell.cs
+++ Assets/Scripts/FireShell.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class FireShell : MonoBehaviour
@@ -10,6 +11,7 @@ public class FireShell : MonoBehaviour
     public Transform turretBase;
     float speed = 15;
     float rotSpeed = 2;
+    float moveSpeed = 3;
 
     // Start is called before the first frame update
     void CreateBullet()
@@ -18,21 +20,22 @@ public class FireShell : MonoBehaviour
         shell.GetComponent<Rigidbody>().velocity = speed * turretBase.forward;
     }
 
-    void RotateTurret()
+    float? RotateTurret()
     {
         float? angle = CalculateAngle(true);
         if(angle != null)
         {
             turretBase.localEulerAngles = new Vector3(360f - (float)angle, 0f, 0f); 
         }
+        return angle;
     }
-
+   
     float? CalculateAngle(bool low)
     {
         Vector3 targetDir = enemy.transform.position - this.transform.position;
         float y = targetDir.y;
         targetDir.y = 0f;
-        float x = targetDir.magnitude;
+        float x = targetDir.magnitude - 1;
         float gravity = 9.8f;
         float sSqr = speed * speed;
         float underTheSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
@@ -60,10 +63,14 @@ public class FireShell : MonoBehaviour
         Vector3 direction = (enemy.transform.position - this.transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0 , direction.z));
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * rotSpeed);
-        RotateTurret();
-       if(Input.GetKeyDown(KeyCode.Space))
+        float? angle = RotateTurret();
+       if(angle != null)
         {
             CreateBullet();
+        }
+       else
+        {
+            this.transform.Translate(0, 0, Time.deltaTime * moveSpeed);
         }
     }
 }
